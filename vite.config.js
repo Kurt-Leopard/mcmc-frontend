786d1693abc7289d3cc163abf62ce33221308dfe
Kurt-Leopard <Kurt-Leopard>
2024-10-29 23:59:14 +0800

    14th commit

--- vite.config.js
+++ vite.config.js
@@ -20,37 +20,37 @@ export default defineConfig({
             "src": "/android/android-launchericon-512-512.png",
             "sizes": "512x512",
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'maskable'
           },
           {
             "src": "/android/android-launchericon-192-192.png",
             "sizes": "192x192",
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'maskable'
           },
           {
             "src": "/android/android-launchericon-144-144.png",
             "sizes": "144x144",
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'maskable'
           },
           {
             "src": "/android/android-launchericon-96-96.png",
             "sizes": "96x96",
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'maskable'
           },
           {
             "src": "/android/android-launchericon-72-72.png",
             "sizes": "72x72",
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'maskable'
           },
           {
             "src": "/android/android-launchericon-48-48.png",
             "sizes": "48x48",
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'maskable'
           },
 
         ],
@@ -66,13 +66,13 @@ export default defineConfig({
         scope: "/",
         screenshots: [
           {
-            src: "/image.png",
+            src: "/android/desktop.png",
             sizes: "1280x720",
             type: "image/png",
             form_factor: "wide"
           },
           {
-            src: "/mobile.png",
+            src: "/android/mobile.png",
             sizes: "640x360",
             type: "image/png"
           }
