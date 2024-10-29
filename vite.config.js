ef795d78531dedbd53ec70898e5684e68910363c
Kurt-Leopard <Kurt-Leopard>
2024-10-30 00:46:27 +0800

    22th commit

--- vite.config.js
+++ vite.config.js
@@ -20,37 +20,37 @@ export default defineConfig({
             "src": "/android/android-launchericon-512-512.png",
             "sizes": "512x512",
             type: 'image/png',
-            purpose: 'any'
+            purpose: 'any maskable'
           },
           {
             "src": "/android/android-launchericon-192-192.png",
             "sizes": "192x192",
             type: 'image/png',
-            purpose: 'any'
+            purpose: 'any maskable'
           },
           {
             "src": "/android/android-launchericon-144-144.png",
             "sizes": "144x144",
             type: 'image/png',
-            purpose: 'any'
+            purpose: 'any maskable'
           },
           {
             "src": "/android/android-launchericon-96-96.png",
             "sizes": "96x96",
             type: 'image/png',
-            purpose: 'any'
+            purpose: 'any maskable'
           },
           {
             "src": "/android/android-launchericon-72-72.png",
             "sizes": "72x72",
             type: 'image/png',
-            purpose: 'any'
+            purpose: 'any maskable'
           },
           {
             "src": "/android/android-launchericon-48-48.png",
             "sizes": "48x48",
             type: 'image/png',
-            purpose: 'any'
+            purpose: 'any maskable'
           },
 
         ],
@@ -62,7 +62,7 @@ export default defineConfig({
         ],
         description: "Join us at the Minister Of Christian Mission Church for fellowship and community service.",
         lang: "en",
-        orientation: "any",
+        orientation: "any maskable",
         scope: "/",
         screenshots: [
           {
